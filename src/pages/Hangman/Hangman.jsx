10aa1cd259d3d9f0b8a27eec3ffecffb78b978ee
Rubcs <Rubcs@gmail.com>
2023-04-25 16:38:48 +0200

    create hangman header and letter buttons

--- src/pages/Hangman/Hangman.jsx
+++ src/pages/Hangman/Hangman.jsx
@@ -1,15 +1,48 @@
 import { Link } from "react-router-dom";
 import "./Hangman.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { hangmanAlphabet } from "../../data/hangmanAlphabet";
+import { hangmanWords } from "../../data/hangmanWords";
 
 const Hangman = () => {
+  const [isStarted, setisStarted] = useState(false);
+  const [word, setWord] = useState("");
+
+  const playButtonHandler = () => {
+    setisStarted(!isStarted);
+    isStarted &&
+      setWord(hangmanWords[Math.floor(Math.random() * hangmanWords.length)]);
+    console.log(isStarted);
+    console.log(word);
+  };
+
   return (
-    <>
-      <p>I'm the Hangman</p>
-      <Link to={`/`}>
-        <button>back home</button>
-      </Link>
-    </>
+    <article className="hangmanWrapper">
+      <div className="homeButtonContainer">
+        <Link to={`/`}>
+          <button className="homeButton">Home</button>
+        </Link>
+      </div>
+
+      <section className="hangmanHeader">
+        <h1 className="title">HANGMAN</h1>
+
+        <button className="playButton" onClick={playButtonHandler}>
+          {isStarted ? "STOP" : "PLAY"}
+        </button>
+      </section>
+
+      <section className="hangmanBody">
+        <section className="hangmanRow"></section>
+        <section className="hangmanAlphabet">
+          {hangmanAlphabet.map((letter) => (
+            <button key={letter} className="letterButton">
+              {letter}
+            </button>
+          ))}
+        </section>
+      </section>
+    </article>
   );
 };
 
