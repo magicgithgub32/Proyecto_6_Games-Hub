f51058c3626961f176b26620fc11559325e15ac7
Rubcs <Rubcs@gmail.com>
2023-05-01 16:28:13 +0200

    fill the sudoku board with random numbers

--- src/pages/Sudoku/Sudoku.jsx
+++ src/pages/Sudoku/Sudoku.jsx
@@ -3,10 +3,24 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Sudoku = () => {
+  const emptyGrid = [];
+  for (let i = 0; i < 9; i++) {
+    emptyGrid.push(new Array(9).fill(null));
+  }
+
   const [isStarted, setisStarted] = useState(false);
+  const [grid, setGrid] = useState(emptyGrid);
+
+  const fillBoard = () => {
+    const newGrid = grid.map((row) =>
+      row.map((cell) => Math.floor(Math.random() * 9) + 1)
+    );
+    setGrid(newGrid);
+  };
 
   const playButtonHandler = () => {
     if (!isStarted) {
+      fillBoard();
     }
     setisStarted(!isStarted);
   };
@@ -29,7 +43,24 @@ const Sudoku = () => {
 
       {isStarted && (
         <section className="sudokuBody">
-          <div className="sudokuBoard"></div>
+          <div className="sudokuBoard">
+            {(() => {
+              const cells = [];
+              for (let i = 0; i < 81; i++) {
+                const row = Math.floor(i / 9);
+                const col = i % 9;
+                cells.push(
+                  <input
+                    key={i}
+                    className="sudokuCell"
+                    value={grid[row][col] || ""}
+                    readOnly
+                  />
+                );
+              }
+              return cells;
+            })()}
+          </div>
         </section>
       )}
     </article>
