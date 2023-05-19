6c2722494771ac6863cef9d2d80945b1f5d4f327
Rubcs <Rubcs@gmail.com>
2023-05-19 16:52:36 +0200

    create useAuthLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from "./pages/Home/Home";
 import TicTacToe from "./pages/TicTacToe/TicTacToe";
 import Hangman from "./pages/Hangman/Hangman";
 import Sudoku from "./pages/Sudoku/Sudoku";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/tictactoe" element={<TicTacToe />}></Route>
         <Route path="/hangman" element={<Hangman />}></Route>
         <Route path="/sudoku" element={<Sudoku />}></Route>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+import "./Login.css";
+import React from "react";
+
+const Login = () => {
+  return <>Login</>;
+};
+
+export default Login;
