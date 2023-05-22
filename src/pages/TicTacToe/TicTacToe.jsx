9d6ef585ea754dba7789c96e0ab29e60eb17a3b5
Rubcs <Rubcs@gmail.com>
2023-05-22 13:12:39 +0200

    amend checkGame - tictactoe route

--- src/pages/TicTacToe/TicTacToe.jsx
+++ src/pages/TicTacToe/TicTacToe.jsx
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 import TictactoeBoardContainer from "../../components/TicTacToe/tictactoeBoardContainer/TicTacToeBoardContainer";
 import BackHome from "../../components/backHome/BackHome";
 import PlayButton from "../../components/TicTacToe/PlayButton";
-import { checkGame } from "../../utils/ticTacToe/checkGame.jsx";
 import GameTitle from "../../components/gameTitle/GameTitle";
+import { checkGame } from "../../utils/ticTacToe/checkGame";
 
 const TicTacToe = () => {
   const [isStarted, setisStarted] = useState(false);
